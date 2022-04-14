b051fe8bcbcc2a742e51e631fe53cbb23359dd99
Shib <contact@shib.cc>
2022-04-14 12:02:39 +0200

    Add (Core > menu) -> Added menu items for viewmodel

--- golphook/menu.v
+++ golphook/menu.v
@@ -3,6 +3,7 @@ module golphook
 import utils
 import math
 import clipboard
+import os
 
 import offsets
 
@@ -24,6 +25,7 @@ pub mut:
 }
 
 fn (mut m Menu) item_bool(name string ,mut value &bool) {
+	mut app_ctx := unsafe { app() }
 	mut color := utils.color_rbga(255,255,255,255)
 	if m.selected == m.items_count {
 		color = utils.color_rbga(108, 92, 231, 255)
@@ -33,12 +35,13 @@ fn (mut m Menu) item_bool(name string ,mut value &bool) {
 		value = !(*value)
 	}
 
-	new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name:", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color).draw()
-	new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 130).vec_3(), "${*value}", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, utils.color_rbga(255,255,255,255)).draw()
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name:", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color))
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 130).vec_3(), "${*value}", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, utils.color_rbga(255,255,255,255)))
 	m.items_count++
 }
 
 fn (mut m Menu) item_i<T>(name string ,mut value &T, step T, min T ,max T) {
+	mut app_ctx := unsafe { app() }
 	mut color := utils.color_rbga(255,255,255,255)
 	if m.selected == m.items_count {
 		color = utils.color_rbga(108, 92, 231, 255)
@@ -57,12 +60,13 @@ fn (mut m Menu) item_i<T>(name string ,mut value &T, step T, min T ,max T) {
 		}
 	}
 
-	new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name:", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color).draw()
-	new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 130).vec_3(), "${f32(*value)}", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, utils.color_rbga(255,255,255,255)).draw()
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name:", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color))
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 130).vec_3(), "${f32(*value)}", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, utils.color_rbga(255,255,255,255)))
 	m.items_count++
 }
 
 fn (mut m Menu) item_pair<T>(name string ,mut value &T, pairs []map[u32]string) {
+	mut app_ctx := unsafe { app() }
 	mut color := utils.color_rbga(255,255,255,255)
 	if m.selected == m.items_count {
 		color = utils.color_rbga(108, 92, 231, 255)
@@ -94,13 +98,62 @@ fn (mut m Menu) item_pair<T>(name string ,mut value &T, pairs []map[u32]string) 
 
 	value = new.keys()[0]
 
-	new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name:", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color).draw()
-	new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 130).vec_3(), "${new[new.keys()[0]]}", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, utils.color_rbga(255,255,255,255)).draw()
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name:", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color))
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 130).vec_3(), "${new[new.keys()[0]]}", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, utils.color_rbga(255,255,255,255)))
+	m.items_count++
+}
+
+fn (mut m Menu) item_pair_callback<T>(name string ,mut value &T, pairs []map[u32]string, callback fn (&App)) {
+	mut app_ctx := unsafe { app() }
+	mut color := utils.color_rbga(255,255,255,255)
+	if m.selected == m.items_count {
+		color = utils.color_rbga(108, 92, 231, 255)
+	}
+
+	mut current_index := 0
+	for i,v in pairs {
+		if *value in v.keys() {
+			current_index = i
+			break
+		}
+	}
+
+	mut new := pairs[current_index].clone()
+
+	if m.selected == m.items_count {
+
+		if m.should_increment {
+			if (current_index + 1) < pairs.len {
+				new = pairs[current_index + 1].clone()
+			}
+		}
+		if m.should_decrement {
+			if (current_index - 1) >= 0 {
+				new = pairs[current_index - 1].clone()
+			}
+		}
+	}
+
+	if m.should_change && m.selected == m.items_count {
+		callback(app_ctx)
+	}
+
+
+	value = new.keys()[0]
+
+	mut selected_color := utils.color_rbga(255,255,255,255)
+	if current_index == app_ctx.config.active_config_idx {
+		selected_color = utils.color_rbga(108, 92, 231, 255)
+	}
+
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name:", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color))
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 130).vec_3(), "${new[new.keys()[0]]}", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, selected_color))
 	m.items_count++
 }
 
 fn (mut m Menu) sep(name string) {
-	new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "- $name", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, utils.color_rbga(255,255,255,255)).draw()
+	mut app_ctx := unsafe { app() }
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "- $name", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, utils.color_rbga(255,255,255,255)))
 	m.relative_menu_pos.x += 8
 }
 
@@ -111,17 +164,17 @@ fn (mut m Menu) tab() {
 }
 
 fn (mut m Menu) item_callback(name string , callback fn (&App)) {
+	mut app_ctx := unsafe { app() }
 	mut color := utils.color_rbga(255,255,255,255)
 	if m.selected == m.items_count {
 		color = utils.color_rbga(108, 92, 231, 255)
 	}
 
 	if m.should_change && m.selected == m.items_count {
-		mut app_ctx := unsafe { app() }
 		callback(app_ctx)
 	}
 
-	new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color).draw()
+	app_ctx.rnd_queue.push(new_text(m.relative_menu_pos + utils.new_vec2(m.relative_menu_pos.x + (m.items_count - m.tab_items_count)*15, 0).vec_3(), "$name", 12, true, true, C.DT_LEFT | C.DT_NOCLIP, color))
 	m.items_count++
 }
 fn (mut m Menu) handle_keys() {
@@ -185,6 +238,13 @@ fn (mut m Menu) render() {
 		m.sep("secret")
 		m.item_bool("killsound", mut &app_ctx.config.active_config.killsound)
 	}
+	m.item_bool("viewmodel", mut &app_ctx.config.active_config.viewmodel_override)
+	if app_ctx.config.active_config.viewmodel_override {
+		m.item_i<f32>("x", mut &app_ctx.config.active_config.viewmodel_override_x, 1, -20, 20)
+		m.item_i<f32>("y", mut &app_ctx.config.active_config.viewmodel_override_y, 1, -20, 20)
+		m.item_i<f32>("z", mut &app_ctx.config.active_config.viewmodel_override_z, 1, -20, 20)
+		m.item_i<f32>("fov", mut &app_ctx.config.active_config.viewmodel_override_fov, 1, -200, 200)
+	}
 
 	m.tab()
 	m.sep("engine")
@@ -195,16 +255,29 @@ fn (mut m Menu) render() {
 
 	m.tab()
 	m.sep("config")
+	mut configs := []map[u32]string{}{}
+	for i, c in app_ctx.config.configs {
+		configs << {u32(i): c.name}
+	}
+	m.item_pair_callback<u32>("configs", mut &app_ctx.config.selected_config_in_menu, configs, fn (mut app_ctx &App) {
+		app_ctx.config.change_to(int(app_ctx.config.selected_config_in_menu))
+	})
 
 	m.item_callback("import", fn (mut app_ctx &App) {
 		mut c := clipboard.new()
-		app_ctx.config.load(c.get_text())
+		app_ctx.config.import_fc(c.get_text())
 	})
 	m.item_callback("export", fn (mut app_ctx &App) {
 		mut c := clipboard.new()
-		exported_cfg := app_ctx.config.export()
+		exported_cfg := app_ctx.config.export(int(app_ctx.config.selected_config_in_menu))
 		unsafe { utils.msg_c(utils.color_rbga(108, 92, 231, 255), exported_cfg) }
 		c.copy(exported_cfg)
+	})
+	m.item_callback("save", fn (mut app_ctx &App) {
+		app_ctx.config.save()
+	})
+	m.item_callback("delete", fn (mut app_ctx &App) {
+		app_ctx.config.delete(int(app_ctx.config.selected_config_in_menu))
 	})
 
 	m.handle_keys()
