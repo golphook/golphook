bb9852477434f436f668c5178bb59c3736a1dd98
Shib <contact@shib.cc>
2022-04-14 12:04:06 +0200

    Add (Core > utils) -> Added new errors about config

--- golphook/utils/errors.v
+++ golphook/utils/errors.v
@@ -8,6 +8,9 @@ const errors = {
 	'Error with a minhook fn':      4
 	"Failed to find window with name": 5
 	"D3D failed to create drawing component": 6
+	"Failed to create ressource configs": 7
+	"Failed to access ressource configs": 8
+	"Failed to resolve resolve sig": 9
 }
 
 pub fn error_critical(withError string, andErrorComplement string) {
--- v.mod
+++ v.mod
@@ -1,7 +1,7 @@
 Module {
 	name: 'golphook'
 	description: 'Cool golp in your game :)'
-	version: '0.35.3-alpha'
+	version: '0.35.4-alpha'
 	license: 'MIT'
 	dependencies: []
 }
