564c04e655b6affd041117f1f6eca886b0328cb2
Shib <contact@shib.cc>
2022-04-22 17:37:46 +0200

    Fix (Core > utils) -> Fixed radian not public

--- golphook/utils/math.v
+++ golphook/utils/math.v
@@ -2,7 +2,7 @@ module utils
 
 import math
 
-const radian = math.pi / 180.0
+pub const radian = math.pi / 180.0
 
 pub fn distance_from(firstEnt Vec3, toEnt Vec3) f32 {
 	return f32(math.sqrt(math.pow(firstEnt.x - toEnt.x, 2) + math.pow(firstEnt.y - toEnt.y, 2) + math.pow(firstEnt.z - toEnt.z, 2)))
