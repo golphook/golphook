d060230e7751a9cdbb0c7a5a16c9b9dda932a173
Shib <contact@shib.cc>
2022-04-22 17:35:08 +0200

    Add (Core > valve) -> Added new file for entity related fn which cannot be in the struct
    because entity struct is in module valve which is imported by app the some fns need app_ctx

--- golphook/entity_fns.v
+++ golphook/entity_fns.v
@@ -2,6 +2,8 @@ module golphook
 
 import valve
 import utils
+import math
+import offsets
 
 pub fn i_can_see(player &valve.Entity, bones []usize) bool {
 
@@ -37,4 +39,53 @@ pub fn check_ent_visible_by_mask(ent &valve.Entity) bool {
 		return true
 	}
 	return false
+}
+
+pub fn my_speed_angles() utils.Vec3 {
+	mut app_ctx := unsafe { app() }
+	v_vel := app_ctx.ent_cacher.local_player.velocity()
+	a_angles := utils.new_angle(0,0,0)
+	app_ctx.interfaces.cdll_int.get_view_angle(&a_angles)
+
+	mut v_final := utils.new_vec3(0,0,0)
+	view_angle_rad := a_angles.yaw * utils.radian
+
+	v_final.x = ( math.cosf(view_angle_rad) * v_vel.x ) - ( math.sinf(view_angle_rad) * ( -v_vel.y ) )
+	v_final.y = ( math.sinf(view_angle_rad) * v_vel.x ) + ( math.cosf(view_angle_rad) * ( -v_vel.y ) )
+	return v_final
+}
+
+pub fn stopp()  {
+	mut app_ctx := unsafe { app() }
+
+	// zz := C.GetAsyncKeyState(0x5A) > 1
+	// qq := C.GetAsyncKeyState(0x51) > 1
+	// ss := C.GetAsyncKeyState(0x53) > 1
+	// dd := C.GetAsyncKeyState(0x44) > 1
+
+	mut force_forward := utils.get_val_offset<int>(app_ctx.h_client, offsets.db.signatures.force_forward)
+	mut force_backward := utils.get_val_offset<int>(app_ctx.h_client, offsets.db.signatures.force_backward)
+	mut force_left := utils.get_val_offset<int>(app_ctx.h_client, offsets.db.signatures.force_left)
+	mut force_right := utils.get_val_offset<int>(app_ctx.h_client, offsets.db.signatures.force_right)
+
+	v_vel := app_ctx.ent_cacher.local_player.velocity()
+	vel := math.sqrtf(v_vel.x * v_vel.x + v_vel.y * v_vel.y)
+	v_avel := my_speed_angles()
+
+	if vel >=30&& ( app_ctx.ent_cacher.local_player.flags() == 257 ) {
+
+		if v_avel.x >= 30 {
+			unsafe { *force_backward = 6 }
+		}
+		if v_avel.x <= -30 {
+			unsafe { *force_forward = 6 }
+		}
+		if v_avel.y >= 30 {
+			unsafe { *force_left = 6 }
+		}
+		if v_avel.y <= -30 {
+			unsafe { *force_right = 6 }
+		}
+
+	}
 }
